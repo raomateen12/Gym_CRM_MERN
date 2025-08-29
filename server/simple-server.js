e247718fa70d78e79fa2a1c500717f57be2525c2
Mateen Ahmad <167166133+raomateen12@users.noreply.github.com>
2025-08-29 19:26:49 +0500

    Update simple-server.js

--- server/simple-server.js
+++ server/simple-server.js
@@ -19,7 +19,7 @@ app.use(cors({
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('🗄️  MongoDB Connected');
+    console.log(' MongoDB Connected');
   })
   .catch(err => {
     console.error('MongoDB connection error:', err);
@@ -43,5 +43,5 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
